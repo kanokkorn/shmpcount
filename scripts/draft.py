818091f63332bb32aa4c93c2bf973da8e04f3766
kanokkorn <kanokorn@outlook.jp>
2020-03-23 18:28:37 +0700

    draft

---
+++ scripts/draft.py
@@ -0,0 +1,10 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import pyqtSlot
+import cv2
+import pathlib
+import time
+import timeit
+
+if __name__ == "__main__":
+  pass
